43fbaeaf2614d1dd00445b06b0fcff165725b048
Lukasz Budnik <l.budnik@kainos.com>
2012-05-26 21:55:37 +0200

    added new Mvc project

---
+++ api/Remindr.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Remindr.Mvc.Models;
+
+namespace Remindr.Mvc.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public JsonResult RemindrTest()
+        {
+            var response = new Response {Success = true};
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
