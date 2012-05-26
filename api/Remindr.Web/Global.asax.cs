58ac7c05cbbfa99fc3f0fd9c59ffd3578bad8e63
Ruairi Quinn <ruairiquinn@hotmail.com>
2012-05-26 15:43:53 +0100

    Add basic scheduling functionality

---
+++ api/Remindr.Web/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Quartz;
+using Quartz.Impl;
+using api.Scheduler;
+
+namespace api
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            ISchedulerFactory factory = new StdSchedulerFactory();
+            var scheduler = factory.GetScheduler();
+            scheduler.Start();
+
+            // Change this later to check for all implementations of a marker iterface like IScheduleJobs
+            var perMinuteScheduler = new PerMinuteScheduler();
+            perMinuteScheduler.Schedule(scheduler);
+        }
+
+    }
+}
