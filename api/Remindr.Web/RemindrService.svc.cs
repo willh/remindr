1e29117911007cecd9499dfed226c6a2943ab604
Lukasz Budnik <l.budnik@kainos.com>
2012-05-26 13:48:59 +0200

    stub for RemindrService

---
+++ api/Remindr.Web/RemindrService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace api
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class RemindrService
+    {
+        [WebGet(UriTemplate = "/SendTextMessage/{phoneNumber}/{text}", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        public bool SendTextMessage(string phoneNumber, string text)
+        {
+            return true;
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
