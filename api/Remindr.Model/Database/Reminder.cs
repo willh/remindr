32f3bb202fae2c5d5f1afb3df5ec26259b92c56f
Kyle Thompson <kylethompson86@gmail.com>
2012-05-26 14:25:31 +0100

    Adding Reminder model class

---
+++ api/Remindr.Model/Database/Reminder.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Reminder.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Remindr.Model.Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Reminder
+    {
+        private ObjectId _Id;
+        private string _mobileNumber;
+        private string _message;
+        DateTime _nextScheduledReminder;
+        string _schedule;
+        string[] _customSchedule;
+        DateTime _endReminderDate;
+        
+        public Reminder(string mobileNumber, string message, DateTime nextScheduledReminder, string schedule,
+                             string[] customSchedule, DateTime endReminderDate)
+        {            
+            _mobileNumber = mobileNumber;
+            _message = message;
+            _nextScheduledReminder = nextScheduledReminder;
+            _schedule = schedule;
+            _customSchedule = customSchedule;
+            _endReminderDate = endReminderDate;
+        }
+
+        public void SaveToDb()
+        {
+            MongoCollection<Reminder> reminderCollection = MongoAccess._mongoDatabase.GetCollection<Reminder>("Reminder");
+            reminderCollection.Insert(this);
+        }
+    }
+
+
+}
