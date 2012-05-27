a7968b6b3239d2d8aaf2639211e7d764df4d40e5
Kyle Thompson <kylethompson86@gmail.com>
2012-05-27 10:20:57 +0100

    Tricking appharbour

--- api/Remindr.Test/DatabaseTests.cs
+++ api/Remindr.Test/DatabaseTests.cs
@@ -42,7 +42,7 @@ namespace Remindr.Test
         [TestMethod]
         public void CanDeleteObjectFromDatabase()
         {
-            Reminder testReminder = new Reminder("deleteTestNumber", "test message", DateTime.SpecifyKind(DateTime.Now.Date, DateTimeKind.Utc), "daily", null, DateTime.Now, "appointment");
+            Reminder testReminder = new Reminder("deleteTestNumber", "test message", DateTime.SpecifyKind(DateTime.Now.Date, DateTimeKind.Utc), "daily", null, DateTime.Now.AddDays(20), "appointment");
             testReminder.InsertToDb();
             MongoCollection<Reminder> reminderCollection = MongoAccess.GetReminderCollection();
             var query = Query.EQ("_mobileNumber", "deleteTestNumber");            
