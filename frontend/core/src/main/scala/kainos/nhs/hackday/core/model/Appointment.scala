c1055d2f1779bf45b2453b06a1f8063dff454643
James Hughes <james@yobriefca.se>
2012-05-27 10:43:31 +0100

    removed old files

--- frontend/core/src/main/scala/kainos/nhs/hackday/core/model/Appointment.scala
+++
@@ -1,5 +0,0 @@
-package kainos.nhs.hackday.core.model
-
-import java.util.Date
-
-case class Appointment(reminderDate: Date,message: String, mobile: String, oneWeekNotification: Boolean, oneDayNotification: Boolean)
--- frontend/core/src/main/scala/kainos/nhs/hackday/core/model/prescription.scala
+++
@@ -1,5 +0,0 @@
-package kainos.nhs.hackday.core.model
-
-import java.util.Date
-
-case class Prescription(mobile: String, reminderdate: Date, message: String)
