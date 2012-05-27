fbcb58f6de0a0634d2b50ef8c6736829bafde19b
Will Hamill <william.hamill@gmail.com>
2012-05-27 10:16:26 +0100

    Updated medication case class, removed custom schedule radio for now

--- frontend/web/app/controllers/Medications.scala
+++ frontend/web/app/controllers/Medications.scala
@@ -1,13 +1,48 @@
 package controllers
 
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import kainos.nhs.hackday.core.model._
 
 object Medications extends Controller {
+
+  val medication = Form(
+    mapping(
+      "mobile" -> nonEmptyText,
+      "reminderstart" -> date("dd-mm-yyyy"),
+      "reminderend" -> date("dd-mm-yyyy"),
+      "schedule" -> nonEmptyText,
+      "message" -> nonEmptyText
+    )(Medication.apply)(Medication.unapply)
+  )
 
   def index = Action { implicit request =>
     Ok(views.html.medications.index())
   }
 
-  def reminder = TODO
+  def reminder = Action { implicit request =>
+    medication.bindFromRequest.fold(
+      errors => {
+        Redirect(routes.Medications.index()).flashing(
+          "error" -> "Could not bind form due to error"
+        )
+      },
+      meds => {
+        sendScheduleRequest(meds)
+        Redirect(routes.Medications.index()).flashing(
+          "message" -> "Successfully scheduled medication reminder"
+        )
+      }
+    )
+  }
+
+  def sendScheduleRequest(med: Medication) {
+    // call API to:
+    //  create scheduled SMS to be sent on daily or weekly basis
+    //  from start date to end date
+    //  with body of reminder message as text
+    //  to recipient of mobile number
+  }
 
 }
