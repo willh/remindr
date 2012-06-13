ac72512cf98495bacc45aabccf650329bf7576cc
James Hughes <james@yobriefca.se>
2012-06-13 16:43:31 +0100

    started rewriting the api

---
+++ api/Remindr.Api/Modules/HistoryModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Remindr.Model;
+using Nancy;
+
+namespace Remindr.Api.Modules
+{
+    public class HistoryModule : Nancy.NancyModule
+    {
+        public HistoryModule() : base("history")
+        {
+            Get["/"] = parameters => Response.AsJson(Reminders.GetAllSentReminders());
+
+            Get["/{number}"] = parameters =>
+            {
+                string number = "+44" + parameters.number.TrimStart('0');
+                return Response.AsJson(Reminders.GetHistoryForNumber(number));
+            };
+        }
+    }
+}
---
+++ api/Remindr.Api/Modules/ReminderModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Remindr.Twilio;
+
+namespace Remindr.Api.Modules
+{
+    public class ReminderModule : NancyModule
+    {
+        public ReminderModule()
+        {
+            TextService service = new TextService();
+
+            Get["/message/send"] = parameters =>
+            {
+                string sendTo = parameters.sendTo;
+                string textMessage = parameters.textMessage;
+
+                try
+                {
+                    service.SendMessage(sendTo, textMessage);
+                    return Response.AsJson(new { success = true });
+                }
+                catch
+                {
+                    return Response.AsJson(new { success = false });
+                }
+            };
+        }
+    }
+}
