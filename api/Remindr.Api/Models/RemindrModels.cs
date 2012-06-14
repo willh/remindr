266ac7d46c496ac117d3d0dd103d828c3cb2c2b9
James Hughes <james@yobriefca.se>
2012-06-14 08:12:18 +0100

    Nancy API tweak

---
+++ api/Remindr.Api/Models/RemindrModels.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Remindr.Api.Models
+{
+
+    public class Response
+    {
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+
+    public class SendTextMessageRequest
+    {
+        public string SendTo { get; set; }
+        public string TextMessage { get; set; }
+    }
+
+    public class Prescription
+    {
+        public string mobileNumber { get; set; }
+        public string reminderDate { get; set; }
+        public string message { get; set; }
+
+    }
+
+    public class Medication
+    {
+        public string mobileNumber { get; set; }
+        public string reminderStartDate { get; set; }
+        public string reminderEndDate { get; set; }
+        public string schedule { get; set; }
+        public string message { get; set; }
+    }
+
+    public class GetHistoryRequest
+    {
+        public string Id { get; set; }
+        public string Number { get; set; }
+    }
+
+    public class Appointment
+    {
+        public string reminderDate { get; set; }
+        public string message { get; set; }
+        public string mobile { get; set; }
+        public bool oneWeekNotification { get; set; }
+        public bool oneDayNotification { get; set; }
+    }
+
+}
--- api/Remindr.Api/Modules/ReminderModule.cs
+++ api/Remindr.Api/Modules/ReminderModule.cs
@@ -3,31 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Nancy;
+using Nancy.ModelBinding;
+using Remindr.Api.Models;
+using Remindr.Model.Database;
 using Remindr.Twilio;
 
 namespace Remindr.Api.Modules
 {
     public class ReminderModule : NancyModule
     {
+        TextService service = new TextService();
+        
         public ReminderModule()
         {
-            TextService service = new TextService();
+            Get["/message/send"] = SendMessage;
 
-            Get["/message/send"] = parameters =>
+            Post["/medication"] = SetupMedicationReminder;
+        }
+
+        private static DateTime ParseDate(string date)
+        {
+            string[] dateParts = date.Split('/');
+            return new DateTime(int.Parse(dateParts[2]), int.Parse(dateParts[1]), int.Parse(dateParts[0]));
+        }
+
+        private Nancy.Response SendMessage(dynamic parameters)
+        {
+            string sendTo = parameters.sendTo;
+            string textMessage = parameters.textMessage;
+
+            try
             {
-                string sendTo = parameters.sendTo;
-                string textMessage = parameters.textMessage;
+                service.SendMessage(sendTo, textMessage);
+                return Response.AsJson(new { success = true });
+            }
+            catch
+            {
+                return Response.AsJson(new { success = false });
+            }
+        }
 
-                try
+        private Nancy.Response SetupMedicationReminder(dynamic _)
+        {
+            Medication medication = this.Bind<Medication>();
+
+            try
+            {
+                var reminderStartDate = ParseDate(medication.reminderStartDate);
+                var reminderEndDate = ParseDate(medication.reminderEndDate);
+                var reminder = new Reminder
                 {
-                    service.SendMessage(sendTo, textMessage);
-                    return Response.AsJson(new { success = true });
-                }
-                catch
+                    _nextScheduledReminder = reminderStartDate,
+                    _endReminderDate = reminderEndDate,
+                    _message = medication.message,
+                    _schedule = medication.schedule,
+                    _mobileNumber = medication.mobileNumber
+                };
+
+                reminder.SaveToDb();
+     
+                return Response.AsJson(new { success = true });
+            }
+            catch(Exception e)
+            {
+                return Response.AsJson(new
                 {
-                    return Response.AsJson(new { success = false });
-                }
-            };
+                    success = false,
+                    message = e.Message
+                });
+            }
         }
     }
 }
